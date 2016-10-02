3633b279fc2ad4dcf8c9c2427a0032cf816e494e
Duncan Hughes <duncster@gmail.com>
2016-10-02 09:16:26 +0100

    started with sprites

--- demo.py
+++ demo.py
@@ -9,19 +9,32 @@ from vec2 import *
 w = 800
 h = 600
 numboids = 25 
-crowd_dist = 20
+crowd_dist = 50 
 close_dist = 200
 
-def display_boid(screen, boid):
-	pygame.draw.line(screen, (255.0, 255.0, 255.0), \
-		(boid.pos.x, boid.pos.y), \
-		(boid.pos.x + boid.vel.x, boid.pos.y + boid.vel.y), 1)
+def display_boid(screen, boid, sprite, frame_num):
+	#pygame.draw.line(screen, (255.0, 255.0, 255.0), \
+	#	(boid.pos.x, boid.pos.y), \
+	#	(boid.pos.x + boid.vel.x, boid.pos.y + boid.vel.y), 1)
+
+	spriterect = sprite.get_rect()
+	spriterect.x = boid.pos.x
+	spriterect.y = boid.pos.y
+
+	sprite.set_clip(pygame.Rect((48 * frame_num) % 384, 0, 48, 48))
+	frame = sprite.subsurface(sprite.get_clip())
+
+	screen.blit(frame, spriterect)
 
 def main():
 	boids = []
 	run = True
 	screen = pygame.display.set_mode((w, h))
 	#screen = pygame.display.set_mode((w, h), pygame.FULLSCREEN|pygame.DOUBLEBUF|pygame.HWSURFACE)
+
+	bird = pygame.image.load("resources/bird.png").convert()
+	bird.set_colorkey((173, 239, 255))
+	frame_num = 0
 
 	for i in range(0, numboids):
 		boids.append(Boid(Vec2(random.randint(0, w), random.randint(0, h))))
@@ -34,12 +47,13 @@ def main():
 				if event.key == pygame.K_ESCAPE:
 					run = False
 
-		screen.fill((0.0, 0.0, 0.0))
+		screen.fill((127.0, 127.0, 127.0))
 
 		move_boids(boids, close_dist, crowd_dist, w, h)
 		
 		for boid in boids:
-			display_boid(screen, boid)
+			display_boid(screen, boid, bird, frame_num)
+			frame_num += 1
 
 		pygame.display.flip()
 		pygame.time.delay(10)
